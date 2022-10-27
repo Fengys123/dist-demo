024f96889000bb4fa74938b41d8afdde4a570904
fys <1113014250@qq.com>
2022-10-27 23:38:08 +0800

    distribute table create and data insert example

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::f64;
+
+use mysql::{prelude::Queryable, OptsBuilder, Pool};
+
+#[derive(Debug, PartialEq)]
+pub struct Monitor {
+    idc: String,
+    host: String,
+    cpu: f64,
+    memory: f64,
+}
+
+fn main() {
+    let create = r"
+CREATE TABLE dist_monitor (
+    idc STRING,
+    host STRING,
+    ts TIMESTAMP DEFAULT current_timestamp(),
+    cpu DOUBLE DEFAULT 0,
+    memory DOUBLE,
+    TIME INDEX (ts),
+    PRIMARY KEY(host))
+PARTITION BY RANGE COLUMNS (idc) (
+    PARTITION p0 VALUES LESS THAN ('host_3000'),
+    PARTITION p1 VALUES LESS THAN ('host_6000'),
+    PARTITION p2 VALUES LESS THAN (MAXVALUE),
+    )
+ENGINE=mito;
+    ";
+
+    let builder = OptsBuilder::new()
+        .ip_or_hostname(Some("127.0.0.1"))
+        .tcp_port(4002)
+        .prefer_socket(false);
+    let pool = Pool::new(builder).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+
+    match conn.query_drop(create) {
+        Ok(_) => println!("create table success!"),
+        Err(e) => println!("create table failed, err: {:?}", e),
+    };
+
+    let count = 90;
+    for i in 0..count {
+        let insert = format! {"INSERT INTO dist_monitor (idc, host, cpu, memory)
+        VALUES (\"idc_{}\", \"host_{}\", 0.2, 0.3)", i, i};
+        match conn.query_drop(insert) {
+            Ok(_) => println!("insert success"),
+            Err(e) => println!("insert failed, err: {:?}", e),
+        }
+    }
+}
